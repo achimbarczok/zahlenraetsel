35e520312744eadf153169ef03a5db04d7ba426e
acb-ct <acb@ct.de>
2020-06-14 22:07:01 +0200

    first upload

---
+++ zahlenraetsel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 Achim Barczok (achim@barczok.de)
+# All rights reserved.
+
+import math
+
+def kombinieren(zahl1,zahl2,operator):
+    #berechnet eine Zahlenkombination per Addition, Subtraktion, Multiplikation, Division oder Potenz
+
+
+    #gibt das Ergebnis aus
+
+def main():
+    #berechnet Zahlenraetsel nach Böer: x ? y = z
+
+    zahl[0] = 1
+    zahl[1] = 2
+
+    #fakultät berücksichtigen!
+    zahl[2] = math.factorial(zahl1)
+    zahl[3] = math.factorial(zahl2)
+
+
+
+    if zahl1 == zahl2:
+        zahlenliste = [int(zahl1)]
+    else:
+        zahlenliste = [int(zahl1), int(zahl2)]
+    print(zahlenliste)
+
+
+main()
